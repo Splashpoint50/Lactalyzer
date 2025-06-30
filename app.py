de3d35fc2d9caf053c970c724250831353ef397f
Splashpoint50 <gilbert.dalit@gmail.com>
2025-06-29 23:25:55 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import os
 from flask import Flask, render_template, request
 import dropbox
 import csv
@@ -59,4 +59,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
